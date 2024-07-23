d0e11cf5259bfb3766aa8c2efdad691396e0aaf5
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-23 17:13:52 +0530

    Add files via upload

---
+++ IndexOf.java
@@ -0,0 +1,13 @@
+package nandini;
+
+public class IndexOf {
+	public static void main(String args[])
+	{
+	 String s="Andra Guru Nandini Reddy";
+	 System.out.println(s.indexOf("Andra"));
+	 System.out.println(s.indexOf('G'));
+	 System.out.println(s.indexOf('a',3));
+	 
+
+}
+}
