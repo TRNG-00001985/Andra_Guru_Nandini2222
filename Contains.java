e16d14b56eab74c9d86478d4bf3432d0f9ca839c
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-23 16:38:22 +0530

    Add files via upload

---
+++ Contains.java
@@ -0,0 +1,13 @@
+package nandini;
+
+public class Contains {
+	public static void  main(String args[])
+	{
+		String s="hello world";
+		
+		System.out.println(s.contains("hello world"));
+	}
+}
+
+
+
