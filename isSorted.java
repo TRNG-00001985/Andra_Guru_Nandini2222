0a73c776762f6ae9183949b731d8c8b1afa52b9d
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-24 19:50:47 +0530

    Add files via upload

---
+++ isSorted.java
@@ -0,0 +1,15 @@
+package nandini;
+import java.util.Arrays;
+
+public class isSorted {
+	public static void main(String args[])
+	{
+		int a[]= {1,2,8,4,5};
+		Arrays.sort(a);
+		//System.out.println(a[i);
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+	}
+	}
+
+}
