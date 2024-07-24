0f8204b50d2ef345bc8032f0f8ff6563d0afe150
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-24 19:50:07 +0530

    Add files via upload

---
+++ MaxminusMin.java
@@ -0,0 +1,14 @@
+package nandini;
+
+public class MaxminusMin {
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=5;
+		int max=a>b?a:b;
+		int min=a<b?a:b;
+		System.out.println("Max minus min value is"+(max-min));
+		
+	}
+
+}
