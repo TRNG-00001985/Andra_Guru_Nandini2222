26bbbeb75a9eb3e02c926de9b54804ac9161d794
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-24 09:37:18 +0530

    Add files via upload

---
+++ GreatestOfThreeNumbers.java
@@ -0,0 +1,22 @@
+package nandini;
+
+public class GreatestOfThreeNumbers {
+	public static void main(String args[])
+	{
+	int a=11;
+	int b=10;
+	int c=32;
+	if(a<b)
+	{
+		System.out.println("a is big");
+		
+	}if(a<c)
+	{
+		System.out.println("c is big");
+	}else
+	{
+		System.out.println("b is big");
+	}
+		
+	}
+}
